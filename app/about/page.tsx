2e73dc0f9d31a6c65330388389f804c90e2ad72f
DmitriyHrushko <d.hrushko@infinitytechnologies.pro>
2023-08-18 14:17:15 +0300

    test

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+	title: "About | Recruitment Platform",
+}
+
+export default function About() {
+	return <h1>Select Subitem</h1>
+}
